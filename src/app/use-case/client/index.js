fc636693c576f9effd16d7e6eddf8dd31dd75954
Francisco Viana <franciscoviana862@gmail.com>
2021-11-06 01:23:10 -0300

    export GetAllClientsUseCase

--- src/app/use-case/client/index.js
+++ src/app/use-case/client/index.js
@@ -1,4 +1,5 @@
 module.exports = {
   CreateClientUseCase: require('./create-client'),
+  GetAllClientsUseCase: require('./get-all-clients'),
   GetByIdClientUseCase: require('./get-by-id-client'),
 };
